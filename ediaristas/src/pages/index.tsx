390b5de0c84e1a546afebd3d35f382ca377ed03f
Lucas Lopes <lucas.lopesx1@gmail.com>
2021-10-11 21:41:48 -0300

    commit second day

--- ediaristas/src/pages/index.tsx
+++ ediaristas/src/pages/index.tsx
@@ -2,26 +2,91 @@ import type { NextPage } from "next";
 import SafeEnvironment from "ui/components/feedback/SafeEnvironment/SafeEnvironment";
 import PageTitle from "ui/components/data-display/PageTitle/PageTitle";
 import UserInformation from "ui/components/data-display/UserInformation/UserInformation";
+import TextFieldMask from "ui/components/inputs/TextFieldMask/TextFieldMask";
+import { Button, Typography, Container } from "@mui/material";
+import {
+  FormELementsContainer,
+  ProfessionalsPaper,
+  ProfessionalsContainer,
+} from "@styles/pages/index.style";
+import { useState } from "react";
 
 const Home: NextPage = () => {
-    return (
-        <div>
-            <SafeEnvironment />
-            <PageTitle
-                title={"Conheça os profissionais"}
-                subtitle={
-                    "Preencha seu endereço e veja todos os profissionais da sua localidade"
-                }
-            />
+  const [contador, setContador] = useState(5);
 
+  return (
+    <div>
+      <SafeEnvironment />
+      <PageTitle
+        title={"Conheça os profissionais"}
+        subtitle={
+          "Preencha seu endereço e veja todos os profissionais da sua localidade"
+        }
+      />
+      Contador: {contador}
+      <button onClick={() => setContador(contador + 1)}>clique aqui</button>
+      <Container>
+        <FormELementsContainer>
+          <TextFieldMask
+            mask={"99.999-999"}
+            label={"Digite seu CEP"}
+            fullWidth
+            variant={"outlined"}
+          />
+
+          <Typography color={"error"}>CEP Invalido</Typography>
+
+          <Button
+            variant={"contained"}
+            color={"secondary"}
+            sx={{ width: "220px" }}
+          >
+            Buscar
+          </Button>
+        </FormELementsContainer>
+        <ProfessionalsPaper>
+          <ProfessionalsContainer>
             <UserInformation
-                name={"Lucas Lopes"}
-                picture={"https://github.com/lucaslopesx.png"}
-                rating={5}
-                description={"Minas Gerais"}
+              name={"Lucas Lopes"}
+              picture={"https://github.com/lucaslopesx.png"}
+              rating={5}
+              description={"Minas Gerais"}
             />
-        </div>
-    );
+            <UserInformation
+              name={"Lucas Lopes"}
+              picture={"https://github.com/lucaslopesx.png"}
+              rating={5}
+              description={"Minas Gerais"}
+            />
+            <UserInformation
+              name={"Lucas Lopes"}
+              picture={"https://github.com/lucaslopesx.png"}
+              rating={5}
+              description={"Minas Gerais"}
+            />
+            <UserInformation
+              name={"Lucas Lopes"}
+              picture={"https://github.com/lucaslopesx.png"}
+              rating={5}
+              description={"Minas Gerais"}
+            />
+            <UserInformation
+              name={"Lucas Lopes"}
+              picture={"https://github.com/lucaslopesx.png"}
+              rating={5}
+              description={"Minas Gerais"}
+            />
+            <UserInformation
+              name={"Lucas Lopes"}
+              picture={"https://github.com/lucaslopesx.png"}
+              rating={5}
+              description={"Minas Gerais"}
+            />
+          </ProfessionalsContainer>
+        </ProfessionalsPaper>
+      </Container>
+    </div>
+  );
 };
 
 export default Home;
--- ediaristas/src/ui/components/data-display/UserInformation/UserInformation.tsx
+++ ediaristas/src/ui/components/data-display/UserInformation/UserInformation.tsx
@@ -1,33 +1,33 @@
 import React from "react";
 import {
-    UserInformationContainer,
-    UserName,
-    UserDescription,
-    AvatarStyled,
-    RatingStyled,
+  UserInformationContainer,
+  UserName,
+  UserDescription,
+  AvatarStyled,
+  RatingStyled,
 } from "./UserInformation.style";
 
 interface UserInformationProps {
-    picture: string;
-    name: string;
-    rating: number;
-    description?: string;
+  picture: string;
+  name: string;
+  rating: number;
+  description?: string;
 }
 
 const UserInformation: React.FC<UserInformationProps> = ({
-    name,
-    picture,
-    rating,
-    description,
+  name,
+  picture,
+  rating,
+  description,
 }) => {
-    return (
-        <UserInformationContainer>
-            <AvatarStyled src={picture}>{name[0]}</AvatarStyled>
-            <UserName>{name}</UserName>
-            <RatingStyled readOnly value={rating} />
-            <UserDescription>{description}</UserDescription>
-        </UserInformationContainer>
-    );
+  return (
+    <UserInformationContainer>
+      <AvatarStyled src={picture}>{name[0]}</AvatarStyled>
+      <UserName>{name}</UserName>
+      <RatingStyled readOnly value={rating} />
+      <UserDescription>{description}</UserDescription>
+    </UserInformationContainer>
+  );
 };
 
 export default UserInformation;
